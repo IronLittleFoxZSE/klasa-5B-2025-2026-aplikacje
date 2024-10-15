930699e221b33aabf81b1b7a5e9a10b80f99de1d
f <f@f.pl>
2024-10-15 10:44:07 +0200

    Rozpoczecie pisania programu Kalkulator z wykorzystaniem MVVM oraz pokazanie właściwości CommandParameter

---
+++ Aplikacje_desktopowe_ i_mobilne/CalculatorMauiApp/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMauiApp.ViewModels
+{
+    public class CalculatorViewModel : BindableObject
+    {
+        private int calculationResult;
+
+        public int CalculationResult
+        {
+            get { return calculationResult; }
+            set
+            {
+                calculationResult = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        private Command numericCommand;
+        public Command NumericCommand
+        {
+            get 
+            {
+                if (numericCommand == null)
+                    numericCommand = new Command<string>((strNumber) => 
+                    {
+                        int digit = int.Parse(strNumber);
+                        CalculationResult = CalculationResult * 10 + digit;
+                    });
+                return numericCommand; 
+            }
+        }
+    }
+}
